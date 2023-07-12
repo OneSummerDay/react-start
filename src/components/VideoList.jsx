045dc2dc0c9c7e046a2671e2ef88b1a846921b55
OneSummerDay <artem.kraievyi@gmail.com>
2023-07-12 16:35:59 +0300

    added content into VideoList component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,5 +1,39 @@
+const videos = [
+    {
+        name: 'Closures from A to Z',
+        duration: '12 minutes',
+    },
+    {
+        name: 'JS Functions',
+        duration: '15 minutes',
+    },
+    {
+        name: 'Array',
+        duration: '8 minutes',
+    },
+]
 export function VideoList(){
     return(
-        <h1>Video List</h1>
+        <>
+            <h1>Video List</h1>
+            {
+                videos.map((video) => {
+                    return(
+                        <div>
+                            <p>
+                                {video.name}
+                            </p>
+                            <p>
+                                {video.duration}
+                            </p>
+                            <button>
+                                Like!
+                            </button>
+                        </div>
+                    )
+                })
+            }
+        </>
+        
     )
 }
